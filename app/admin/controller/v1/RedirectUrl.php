4c9657bfacf0bf17f466b769324a04aaa5e7c18f
yulinzhihou <yulinzhihou@163.com>
2021-12-08 23:07:06 +0800

    12-8 完成短链接口

--- app/admin/controller/v1/RedirectUrl.php
+++ app/admin/controller/v1/RedirectUrl.php
@@ -40,7 +40,7 @@ class RedirectUrl extends Base
             if (Cache::has($adminId.'-access_token')) {
                 $acToken = Cache::get($adminId.'-access_token');
             } else {
-                $this->url = 'https://api.weixin.qq.com/cgi-bin/token';//请求url
+                $url = 'https://api.weixin.qq.com/cgi-bin/token?';//请求url
                 $paramsArray = [
                     //微信授权类型,官方文档定义为 ： client_credential
                     'grant_type' => 'client_credential',
@@ -49,8 +49,8 @@ class RedirectUrl extends Base
                     //你的秘钥
                     'secret' => $appConfig['app_secret']
                 ];
-                $this->setParams = http_build_query($paramsArray);//生成URL参数字符串
-                $accessToken = json_decode($this->getTokenCurl(),true);
+                $params = http_build_query($paramsArray);//生成URL参数字符串\
+                $accessToken = $this->http($url.$params,[]);
                 if (isset($accessToken['access_token'])) {
                     //表示请求成功
                     $acToken = $accessToken['access_token'];
@@ -60,61 +60,25 @@ class RedirectUrl extends Base
 
             // 获取token 缓存起来，再获取url_short
             $urlData = [
-                'page'          => 'pages/index/index',
+                'path'          => 'pages/index/index',
                 'query'         => 'id=1&admin_id=1',
                 'is_expire'     =>  true,
                 'expire_type'   => 1,
                 'expire_interval' => 1,
-                'expire_time'   => 180 * 3600 * 24,
-                'env_release'   =>  'develop',/*release,develop,trial*/
+                /*release,develop,trial*/
+                'env_version'   =>  'release'
             ];
 
-
             $this->url = "https://api.weixin.qq.com/wxa/generate_urllink?access_token=".$acToken;
-            $this->setParams = $urlData;
 
-            $shortUrl = json_decode($this->getTokenCurl(true),true);
-//            $shortUrl = $this->http($this->url,$urlData,true);
-            Cache::set('short_url',$shortUrl,300);
-
-            return $this->jsonR('获取成功',$shortUrl);
+            $shortUrl = $this->http($this->url,$urlData,true);
+            if (isset($shortUrl['errcode']) && $shortUrl['errcode'] == 0) {
+                $url = ['url' => $shortUrl['url_link']];
+            } else {
+                $url = [];
+            }
+            return $this->jsonR('获取成功',$url);
         }
         return $this->jsonR('小程序配置不正确，请检查后再进行获取');
-
-
-    }
-
-
-    public function getTokenCurl($ispost = false)
-    {
-        $httpInfo = array();
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36');
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        if ($ispost) {
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $this->setParams);
-            curl_setopt($ch, CURLOPT_URL, $this->url);
-        } else {
-            if ($this->setParams) {
-                curl_setopt($ch, CURLOPT_URL, $this->url . '?' . $this->setParams);
-            } else {
-                curl_setopt($ch, CURLOPT_URL, $this->url);
-            }
-        }
-        $response = curl_exec($ch);
-        if ($response === FALSE) {
-            return false;
-        }
-        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        $httpInfo = array_merge($httpInfo, curl_getinfo($ch));
-        Cache::set('http-info',$httpInfo,200);
-        Cache::set('http-code',$httpCode,200);
-        Cache::set('response',$response,300);
-        curl_close($ch);
-        return $response;
     }
 }
